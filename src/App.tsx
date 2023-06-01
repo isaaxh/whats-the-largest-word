17b67258ef7ad53b73098084e3f993528e5f40b6
Isaac Hussain <isaaclearns58@gmail.com>
2023-06-01 10:24:00 +0300

    add strategy to solve the problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 
+// break the sentence down to an array of words
+// then cycle through each word of the array
+// then create another array which will contain the length of each word
+// then note down the length of each word inside that array
+// then return the highest number from the indices
+// then find the corresponding word of the same index in the word array
+
 const App = () => {
+  const [output, setOutput] = useState("");
+
   return (
     <div className='container'>
       <div className='question-container'>
-        <h1 className='title'>Palindrome Checker</h1>
+        <h1 className='title'>Largest word?</h1>
         <div className='input-container'>
           <input
             className='input-text'
             type='text'
-            placeholder='Enter a text here...'
+            placeholder='Enter a sentence here...'
           />
         </div>
       </div>
       <div className='output-container'>
         <h1 className='title'>Output</h1>
-        <div className='output'></div>
+        <div className='output'>{output}</div>
       </div>
       <div className='btn-container'>
         <button className='btn' type='button'>
