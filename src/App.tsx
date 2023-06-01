5c4418ec4bdad7ddc8909bd3e4c6885e4253ba76
Isaac Hussain <isaaclearns58@gmail.com>
2023-06-01 10:17:43 +0300

    add code from previous project

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='container'>
+      <div className='question-container'>
+        <h1 className='title'>Palindrome Checker</h1>
+        <div className='input-container'>
+          <input
+            className='input-text'
+            type='text'
+            placeholder='Enter a text here...'
+          />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className='output-container'>
+        <h1 className='title'>Output</h1>
+        <div className='output'></div>
+      </div>
+      <div className='btn-container'>
+        <button className='btn' type='button'>
+          Check
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
