b6e2ca566b8237d2ab85aecd4de51c6aef7d925a
Isaac Hussain <isaaclearns58@gmail.com>
2023-06-01 11:46:41 +0300

    comment out the other people's code

--- src/App.tsx
+++ src/App.tsx
@@ -14,60 +14,60 @@ const App = () => {
   const [numOfChar, setNumOfChar] = useState<number[]>([]);
   const [output, setOutput] = useState<string>("");
 
-  // const breakStringIntoArray = (str: string) => {
-  //   const newArr = str.split(" ");
+  const breakStringIntoArray = (str: string) => {
+    const newArr = str.split(" ");
 
-  //   setWordArr(newArr);
-  // };
+    setWordArr(newArr);
+  };
 
-  // const findNumOfChar = (arrOfWord: string[]) => {
-  //   const newArr: number[] = [];
-  //   arrOfWord.forEach((word) => {
-  //     newArr.push(word.length);
-  //   });
+  const findNumOfChar = (arrOfWord: string[]) => {
+    const newArr: number[] = [];
+    arrOfWord.forEach((word) => {
+      newArr.push(word.length);
+    });
 
-  //   setNumOfChar(newArr);
-  // };
+    setNumOfChar(newArr);
+  };
 
-  // const findTheLargestWord = (arrOfWord: string[], arrOfCharNum: number[]) => {
-  //   const largestNum = Math.max.apply(Math, arrOfCharNum);
-  //   const indexOfLargestNum = arrOfCharNum.indexOf(largestNum);
+  const findTheLargestWord = (arrOfWord: string[], arrOfCharNum: number[]) => {
+    const largestNum = Math.max.apply(Math, arrOfCharNum);
+    const indexOfLargestNum = arrOfCharNum.indexOf(largestNum);
 
-  //   const largestWord = arrOfWord[indexOfLargestNum];
+    const largestWord = arrOfWord[indexOfLargestNum];
 
-  //   setOutput(`The largest word is "${largestWord}"`);
-  // };
+    setOutput(`The largest word is "${largestWord}"`);
+  };
 
-  // useEffect(() => {
-  //   breakStringIntoArray(inputText);
-  // }, [inputText]);
+  useEffect(() => {
+    breakStringIntoArray(inputText);
+  }, [inputText]);
 
-  // useEffect(() => {
-  //   findNumOfChar(wordArr);
-  // }, [wordArr]);
+  useEffect(() => {
+    findNumOfChar(wordArr);
+  }, [wordArr]);
 
   const handleBtnClick = (str: string) => {
-    // if (wordArr.length !== 0 && numOfChar.length !== 0) {
-    //   findTheLargestWord(wordArr, numOfChar);
-    // }
-
-    const longestWord = theLargestWord(str);
-    setOutput(`The largest word is "${longestWord}"`);
-  };
-
-  const theLargestWord = (str: string): string => {
-    const strArr: string[] = str.split(" ");
-    let largest = 0;
-    let word = "";
-
-    for (let i = 0; i < strArr.length; i++) {
-      if (largest < strArr[i].length) {
-        largest = strArr[i].length;
-        word = strArr[i];
-      }
+    if (wordArr.length !== 0 && numOfChar.length !== 0) {
+      findTheLargestWord(wordArr, numOfChar);
     }
-    return word;
+
+    // const longestWord = theLargestWord(str);
+    // setOutput(`The largest word is "${longestWord}"`);
   };
+
+  // const theLargestWord = (str: string): string => {
+  //   const strArr: string[] = str.split(" ");
+  //   let largestNumOfChar = 0;
+  //   let word = "";
+
+  //   for (let i = 0; i < strArr.length; i++) {
+  //     if (largestNumOfChar < strArr[i].length) {
+  //       largestNumOfChar = strArr[i].length;
+  //       word = strArr[i];
+  //     }
+  //   }
+  //   return word;
+  // };
 
   return (
     <div className='container'>
